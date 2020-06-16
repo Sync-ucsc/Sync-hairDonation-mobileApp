e3a621d8d781ff585da1d23d41063d2288bce140
SachinduHimash <himash1997@gmail.com>
2020-06-16 11:59:06 +0530

    Added a custom app bar

--- sync_mobile_app/lib/screens/dashboard.dart
+++ sync_mobile_app/lib/screens/dashboard.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:sync_mobile_app/custom_app_bar.dart';
 
 class DashBoard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _height=MediaQuery.of(context).size.height;
+    final _width=MediaQuery.of(context).size.width;
+
+
     return Scaffold(
-     
+     body: SingleChildScrollView(
+       child:Column(children: <Widget>[
+         
+           
+         
+       ],)
+     ),
       
     );
   }
