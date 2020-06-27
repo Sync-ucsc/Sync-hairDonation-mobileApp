f4c3859d80b24b5e39591957876fae99ffc1b4e7
SachinduHimash <himash1997@gmail.com>
2020-06-27 06:08:23 +0530

    Added google maps to the app

--- sync_mobile_app/lib/screens/routes_view.dart
+++ sync_mobile_app/lib/screens/routes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class RoutesView extends StatefulWidget {
   @override
@@ -6,10 +7,30 @@ class RoutesView extends StatefulWidget {
 }
 
 class _RoutesViewState extends State<RoutesView> {
+ 
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(6.8372, 79.9203);
+
+
+
+
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+
+        return Scaffold(
+          body:GoogleMap(
+         
+       onMapCreated: _onMapCreated,
+       initialCameraPosition: CameraPosition(
+       target: _center,
+       zoom: 13.0,)
+      )
     );
   }
 }
